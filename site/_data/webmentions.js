1edfdff23a12a9c52205af23a5120dd948d3a474
Jason <126845456+reddmo@users.noreply.github.com>
2025-02-15 22:00:19 -0500

    Update webmentions.js

--- site/_data/webmentions.js
+++ site/_data/webmentions.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export default async function () {
 const WEBMENTIONS_STUFF = process.env.WEBMENTION_IO_TOKEN;
-const url = `https://webmention.io/api/mentions.jf2?token=${WEBMENTIONS_STUFF}&per-page=900`;
+const url = "https://webmention.io/api/mentions.jf2?token=${WEBMENTIONS_STUFF}&per-page=900";
 const res = await EleventyFetch(url, {
     duration: "1h",
     type: "json",
